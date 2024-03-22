ba5713a354bbbadb744080e9a35bbbab8f937a1c
Celoma <salouet.clement049@gmail.com>
2024-03-22 14:18:14 +0100

    Essaye de compréhension next.js

--- tech-no-limits/src/app/page.tsx
+++ tech-no-limits/src/app/page.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <p className="text-3xl font-bold underline mt-10">Matéo est ma pute</p>
     </main>
   );
 }
