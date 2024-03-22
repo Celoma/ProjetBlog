9a4ac9b522593603f52e355a2e702444f06491c3
unknown <mateobouvier@gmail.com>
2024-03-22 14:08:52 +0100

    commit premier ajout Footer.jsx

---
+++ tech-no-limits/src/app/components/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+        <>
+            <hr>
+                <div className="footer-container">
+                    <div className="social-icons">
+                        <a 
+                        href="https://twitter.com"
+                        aria-label="Twitter"
+                        target="_blank">
+                        </a>
+                    </div>
+                </div>
+            </hr>
+        </>
+    )
+}
