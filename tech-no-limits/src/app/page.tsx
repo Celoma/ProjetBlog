fe661622241402ffbc2894c1a0824228e4851cc8
Celoma <salouet.clement049@gmail.com>
2024-03-26 13:30:46 +0100

    connexion db effectué

--- tech-no-limits/src/app/page.tsx
+++ tech-no-limits/src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import {prisma} from "@/db/prisma";
+import {prisma} from "../db/prisma"
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
 
@@ -13,25 +13,13 @@ export default async function home() {
         testElement.textContent = "coucou";
       }
   };
-  await prisma.user.create({
-    data:{
-      sex: 0,
-      password: "admin",
-      permission: "admin",
-      username : "admin",
-      email : "admin@admin.com",
-          posts:{
-            create:{
-              title:"My First Post",
-              body:"Lots of interesting stuff",
-              slug:"my-first-post"	    },},},})
-  const allUsers = await prisma.User.findMany({
-    include : { post:true }})
+  const allUsers = await prisma.user.findMany({
+    include : { posts:false }})
   return (
     <main>
       <p id="test">cocou</p>
-      <a href="" onClick={handleClick}>Test</a>
-      <div className="text-center"><ul>{allUsers.map((User: { username: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (<li>{User.username}</li>))}</ul></div>
+      <a href="" onClick={handleClick}>Tdest</a>
+      <div className="text-center"><ul>{allUsers.map((User: { permission: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (<li>{User.permission}</li>))}</ul></div>
     </main>
   );
 }
--- tech-no-limits/src/db/prisma.ts
+++ tech-no-limits/src/db/prisma.ts
@@ -1,15 +1,14 @@
 import {PrismaClient} from "@prisma/client";
 
 declare global {
-   var prisma: PrismaClient | undefined;
-  }
+        var prisma: PrismaClient | undefined;
+    }
 export const prisma =
     global.prisma ??
     new PrismaClient({
-               log:
-                        process.env.NODE_ENV === 'development'
-                                    ? ['query', 'error', 'warn'] : ['error'] });
-
+            log:
+                process.env.NODE_ENV === 'development'
+                            ? ['query', 'error', 'warn'] : ['error'] });
 if (process.env.NODE_ENV !== 'production') {
-    global.prisma = prisma;
-}
+        global.prisma = prisma;
+    }
