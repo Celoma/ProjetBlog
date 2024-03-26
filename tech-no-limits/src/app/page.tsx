7ecb554eb47493494c05cf3bbd39aa95193434e2
Celoma <salouet.clement049@gmail.com>
2024-03-26 14:39:43 +0100

    connexion et inscription

--- tech-no-limits/src/app/page.tsx
+++ tech-no-limits/src/app/page.tsx
@@ -1,25 +1,16 @@
-"use client"
 import {prisma} from "../db/prisma"
 import { ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
-
 
 
 export default async function home() {
 
 
-  const handleClick = () => {
-    const testElement = document.getElementById("test");
-      if (testElement) {
-        testElement.textContent = "coucou";
-      }
-  };
   const allUsers = await prisma.user.findMany({
     include : { posts:false }})
   return (
     <main>
       <p id="test">cocou</p>
-      <a href="" onClick={handleClick}>Tdest</a>
-      <div className="text-center"><ul>{allUsers.map((User: { permission: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => (<li>{User.permission}</li>))}</ul></div>
+      <div className="text-center"><ul>{allUsers.map((User) => (<li>{User.password} {User.permission}</li>))}</ul></div>
     </main>
   );
 }
