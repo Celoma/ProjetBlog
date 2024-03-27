4db4b8ed5fae140e07d770c6da636e7dcc343b51
Celoma <salouet.clement049@gmail.com>
2024-03-27 11:40:24 +0100

    register 2.0

--- tech-no-limits/src/app/api/users/login/route.ts
+++ tech-no-limits/src/app/api/users/login/route.ts
@@ -1,22 +1,22 @@
 import { NextResponse } from "next/server";
 import {prisma} from "@/db/prisma"
 
-export async function POST(request: Request) {
-    const data = await  request.json();
-    console.log(data)
+export  async function POST(request: Request) {
+    const data = await request.json();
     try {
-        const newUser = await prisma.user.create({
-            data: {
-                username: data.username,
+        const user = await prisma.user.findFirst({
+            where: {
                 email: data.email,
-                sex: (data.sex == "femme"),
-                password: data.password,
-                permission: "user"
             },
         });
-        return NextResponse.json(newUser)
+        if (user && data.password === user.password) {
+            return NextResponse.json(user)
+        } else {
+            return NextResponse.json({ message: 'Email ou mot de passe incorrect' });
+        }
     } catch (error) {
-        console.error('Erreur lors de la création de l\'utilisateur :', error);
-        throw error;
+        console.error('Erreur lors de la vérification de l\'utilisateur :', error);
+        return NextResponse.json({ message: 'Erreur lors de la vérification de l\'utilisateur' });
     }
 }
+
