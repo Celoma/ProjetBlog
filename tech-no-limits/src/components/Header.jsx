fd28799936eaea4e8d736bb6cfd0c2c13fc67dcc
Celoma <salouet.clement049@gmail.com>
2024-03-27 13:55:46 +0100

    Commit

--- tech-no-limits/src/components/Header.jsx
+++ tech-no-limits/src/components/Header.jsx
@@ -39,8 +39,6 @@ export { createUser };
 const Header = () => {
 
   let [selectedOption, setSelectedOption] = useState(null);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [username, setUsername] = useState('');
 
   const handleOptionChange = (event) => {
     setSelectedOption(event.target.value);
@@ -157,19 +155,13 @@ addEvent(window, 'load', initEscape);
         </div>
         <img src="/images/logo2.png" alt="Logo" className="h-15 w-auto absolute left-2/4 -translate-x-2/4"/>
         <div>
-          {isLoggedIn ? (
-            <p className="text-white">Bienvenue, {username} !</p>
-          ) : (
-            <>
-              <button onClick={loginClick} className="text-slate-100 mr-2 font-semibold hover:text-gray-400">Connexion</button>
-              <button onClick={signinClick} className="btn relative inline-flex items-center justify-start overflow-hidden transition-all bg-custom-orange rounded hover:bg-custom-orange group mr-5 ml-2 p-2 font-semibold">
-                <span className="w-0 h-0 rounded bg-custom-brown absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
-                <span className="w-full text-black transition-colors duration-300 ease-in-out group-hover:text-white z-10">
-                  Inscription
-                </span>
-              </button>
-            </>
-          )}
+            <button onClick={loginClick} className="text-slate-100 mr-2 font-semibold hover:text-gray-400">Connexion</button>
+            <button onClick={signinClick} className="btn relative inline-flex items-center justify-start overflow-hidden transition-all bg-custom-orange rounded hover:bg-custom-orange group mr-5 ml-2 p-2 font-semibold">
+              <span className="w-0 h-0 rounded bg-custom-brown absolute top-0 left-0 ease-out duration-500 transition-all group-hover:w-full group-hover:h-full -z-1"></span>
+              <span className="w-full text-black transition-colors duration-300 ease-in-out group-hover:text-white z-10">
+                Inscription
+              </span>
+            </button>
         </div>
       </div>
       <div id='signin' className='hidden backdrop-filter backdrop-blur-md absolute w-full h-full z-10 top-0'>
