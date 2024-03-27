d99d0a044a575789b58f31c637219d51e41c0055
unknown <mateobouvier@gmail.com>
2024-03-27 09:56:47 +0100

    commit page categories

--- tech-no-limits/src/components/Header.jsx
+++ tech-no-limits/src/components/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
           <a href="" className="text-slate-100 mx-2 font-semibold hover:text-gray-400">ACTUS</a>
           <a href="" className="text-slate-100 mx-2 font-semibold hover:text-gray-400">CATEGORIES</a> 
         </div>
-        <img src="&/images/logo2.png" alt="Logo" className="h-15 w-auto absolute left-2/4 -translate-x-2/4"/>
+        <img src="/images/logo2.png" alt="Logo" className="h-15 w-auto absolute left-2/4 -translate-x-2/4"/>
         <div> {/* boutons vers pages de log-in sign-in*/}
           <button onClick={loginClick} className="text-slate-100 mr-2 font-semibold hover:text-gray-400">Connexion</button>
 
