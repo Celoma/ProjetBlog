3c1c33e37798ffc6b56aff77b6eaab8cfd4ca22a
Celoma <salouet.clement049@gmail.com>
2024-04-03 13:40:30 +0200

    Ajout component

--- tech-no-limits/src/app/pages/author/[idauthor]/page.js
+++ tech-no-limits/src/app/pages/author/[idauthor]/page.js
@@ -61,6 +61,21 @@ const page = ({ params }) => {
                             <h1 className='ml-2 font-bold text-3xl truncate mb-2'>{Post.title}</h1>
                             {author && <p className='text-sm ml-2 text-[#777777] mb-2'>{author.username}</p>}
                             <p className='ml-2 truncate mb-2'>{Post.body}</p>
+                            <p className='ml-2 mb-2 flex'>
+                                {Post.likes.length}
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    viewBox="0 0 24 24"
+                                    fill="none"
+                                    stroke="currentColor"
+                                    strokeWidth="2"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    className="w-5 h-5 ml-1 mt-0.5"
+                                    >
+                                    <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
+                                </svg>
+                            </p>
                         </a>
                 ))}
             </div>
--- tech-no-limits/src/app/pages/blogList/page.js
+++ tech-no-limits/src/app/pages/blogList/page.js
@@ -2,6 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSession } from 'next-auth/react';
+import Post from '@/components/Post';
 
 export default function Page() {
     const [currentPage, setCurrentPage] = useState(1);
@@ -98,35 +99,17 @@ export default function Page() {
                     </select>
                 </div>
                 <div className="grid grid-cols-3 gap-16 mb-8 mt-16 px-[140PX]">
-                    {filteredArticles.slice(startIndex, endIndex).map((Post, index) => (
+                    {filteredArticles.slice(startIndex, endIndex).map((post, index) => (
                         <React.Fragment key={index}>
-                            <a href={`/pages/blog/${Post.id}`} className='cursor-pointer hover:bg-[#D9D9D9] flex flex-col p-4 bg-white rounded'>                                        
-                                <img 
-                                    src={(Post.images && !Post.images.startsWith("https://bzimuqucq3omfllk.public.blob.vercel-storage.com/undefined-")) ? Post.images : '/images/defaultblog.jpg'} 
-                                     className='max-h-64 w-auto rounded' 
-                                />
-                                <p className='text-sm ml-2 w-min text-white bg-custom-blue p-1 rounded-md mt-4 mb-2'>{Post.theme}</p>
-                                <h1 className='ml-2 font-bold text-3xl truncate mb-2'>{Post.title}</h1>
-                                {allUsers && allUsers.find(user => user.id === Post.authorId) && (
-                                    <p className='text-sm ml-2 text-[#777777] mb-2'>{allUsers.find(user => user.id === Post.authorId)?.username}</p>
-                                )}
-                                <p className='ml-2 truncate mb-2'>{Post.body}</p>
-                                <p className='ml-2 mb-2 flex'>
-                                    {Post.likes.length}
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        viewBox="0 0 24 24"
-                                        fill="none"
-                                        stroke="currentColor"
-                                        strokeWidth="2"
-                                        strokeLinecap="round"
-                                        strokeLinejoin="round"
-                                        className="w-5 h-5 ml-1 mt-0.5"
-                                        >
-                                        <path d="M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z" />
-                                    </svg>
-                                </p>
-                            </a>
+                          <Post
+                            id={post.id}
+                            theme={post.theme}
+                            title={post.title}
+                            authorId={post.authorId}
+                            body={post.body}
+                            likes={post.likes}
+                            allUsers={allUsers}
+                          />
                         </React.Fragment>
                     ))}
                 </div>
